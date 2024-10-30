c52002821ace7364509d4a7965046b4b240064b4
uczen <uczen@ST14.inf15.local>
2024-10-30 10:57:33 +0100

    feat: add initial Java server and client

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,5 +1,24 @@
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("[-] Server is starting");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(8000);
+            System.out.printf("[-] Server properties: %s\n", serverSocket);
+
+            Socket socket = serverSocket.accept();
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            String str = dataInputStream.readUTF();
+
+            System.out.printf("[-] Message: %s\n", str);
+
+            socket.close();
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
     }
 }
