308064c67698f66b0cd0ea7a01d004406e28dbdd
uczen <uczen@ST14.inf15.local>
2024-10-30 10:57:33 +0100

    Stworzenie podstawowego serwera i klienta w Javie

--- Clients/JavaClient/src/Main.java
+++ Clients/JavaClient/src/Main.java
@@ -1,5 +1,23 @@
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("[-] Client is starting");
+
+        try {
+            Socket socket = new Socket("localhost", 8000);
+            System.out.printf("[-] Socket properties: %s", socket);
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            dataOutputStream.writeUTF("Hello!");
+            dataOutputStream.flush();
+
+            dataOutputStream.close();
+            socket.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
