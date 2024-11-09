21981c6b0d9e467622a68b872e5a4d53e6699ed2
Kfiatek430 <110732078+Kfiatek430@users.noreply.github.com>
2024-11-09 21:14:10 +0100

    Zmiana sposobu przesyłania danych, podział serwera na klasy Server i Connection, obsługa wielu połączeń

--- Clients/JavaClient/src/Main.java
+++ Clients/JavaClient/src/Main.java
@@ -1,19 +1,27 @@
-import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("[-] Client is starting");
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
         try {
             Socket socket = new Socket("localhost", 8000);
-            System.out.printf("[-] Socket properties: %s", socket);
-            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            System.out.println(socket);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            dataOutputStream.writeUTF("Hello!");
-            dataOutputStream.flush();
+            String message = "";
+            while(!message.equals("exit")) {
+                message = consoleReader.readLine();
+                writer.write(message + "\n");
+                writer.flush();
+            }
 
-            dataOutputStream.close();
+            writer.close();
             socket.close();
         } catch (Exception e) {
             System.out.println(e);
--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,24 +1,11 @@
-import java.io.DataInputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[-] Server is starting");
-
         try {
-            ServerSocket serverSocket = new ServerSocket(8000);
-            System.out.printf("[-] Server properties: %s\n", serverSocket);
-
-            Socket socket = serverSocket.accept();
-            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-            String str = dataInputStream.readUTF();
-
-            System.out.printf("[-] Message: %s\n", str);
-
-            socket.close();
-        } catch (Exception exception) {
-            System.out.println(exception);
+            new Server();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
