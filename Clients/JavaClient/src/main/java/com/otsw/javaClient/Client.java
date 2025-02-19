55e65cdc422bf2671bea195aa49a124f1b69cdad
Kfiatek430 <110732078+Kfiatek430@users.noreply.github.com>
2025-02-19 23:15:05 +0100

    Rozpoczęcie implementacji metody performHandshake

--- Clients/JavaClient/src/main/java/com/otsw/javaClient/Client.java
+++ Clients/JavaClient/src/main/java/com/otsw/javaClient/Client.java
@@ -6,6 +6,11 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Base64;
+import java.util.Random;
 
 public class Client extends Thread {
     Socket socket;
@@ -13,16 +18,15 @@ public class Client extends Thread {
     BufferedWriter writer;
     BufferedReader reader;
     private static final Logger logger = LogManager.getLogger(Client.class.getName());
+    private int port = -1;
     private boolean shouldContinue = true;
 
     @SneakyThrows
     public Client(int port) {
         logger.info("[-] Client is starting...");
         socket = new Socket("localhost", port);
+        this.port = port;
         logger.info("[-] Client properties: " + socket);
-
-        setupReaders();
-
         setName("Client");
         start();
     }
@@ -33,10 +37,27 @@ public class Client extends Thread {
     }
 
     @SneakyThrows
-    public void setupReaders() {
+    public void setupBuffered() {
         consoleReader = new BufferedReader(new InputStreamReader(System.in));
         writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
         reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    @SneakyThrows
+    public void performHandshake() {
+        setupBuffered();
+
+        writer.write("GET / HTTP/1.1");
+        writer.write("Upgrade:  websocket");
+        writer.write("http://localhost:" + port);
+
+        byte[] array = new byte[16];
+        new Random().nextBytes(array);
+        String encoded = new String(Base64.getEncoder().encode(array));
+        writer.write("Sec-WebSocket-Key: " + encoded);
+
+        writer.write("Sec-WebSocket-Version: 13");
+        writer.write("Connection: Upgrade\n\n");
     }
 
     @lombok.SneakyThrows
