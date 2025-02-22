4969f520211165348bb6238484acec42f7e414e7
Kfiatek430 <110732078+Kfiatek430@users.noreply.github.com>
2025-02-22 20:16:00 +0100

    feat(client): update performHandshake and closeClient methods

--- Clients/JavaClient/src/main/java/com/otsw/javaClient/Client.java
+++ Clients/JavaClient/src/main/java/com/otsw/javaClient/Client.java
@@ -6,76 +6,133 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.*;
 import java.net.Socket;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Client extends Thread {
-    Socket socket;
-    BufferedReader consoleReader;
-    BufferedWriter writer;
-    BufferedReader reader;
-    private static final Logger logger = LogManager.getLogger(Client.class.getName());
-    private int port = -1;
-    private boolean shouldContinue = true;
+  Socket socket;
+  BufferedReader consoleReader;
+  BufferedWriter writer;
+  BufferedReader reader;
+  private static final Logger logger = LogManager.getLogger(Client.class.getName());
+  private int port = -1;
+  private volatile boolean shouldContinue = true;
 
-    @SneakyThrows
-    public Client(int port) {
-        logger.info("[-] Client is starting...");
-        socket = new Socket("localhost", port);
-        this.port = port;
-        logger.info("[-] Client properties: " + socket);
-        setName("Client");
-        start();
+  @SneakyThrows
+  public Client(int port) {
+    logger.info("[-] Client is starting...");
+    socket = new Socket("localhost", port);
+    this.port = port;
+    logger.info("[-] Client properties: " + socket);
+    setName("Client");
+    start();
+  }
+
+  public void run() {
+    if(performHandshake()) {
+      new Thread(this::sendMessages, "Client Sender").start();
+      new Thread(this::reciveMessages, "Client Receiver").start();
+    }
+  }
+
+  @SneakyThrows
+  private void setupBuffered() {
+    consoleReader = new BufferedReader(new InputStreamReader(System.in));
+    writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+  }
+
+  @SneakyThrows
+  boolean performHandshake() {
+    setupBuffered();
+
+    writer.write("GET / HTTP/1.1\n");
+    writer.write("Host: http://localhost:" + port + "\n");
+    writer.write("Upgrade: websocket\n");
+    writer.write("Connection: Upgrade\n");
+
+    byte[] randomBytes = new byte[16];
+    new Random().nextBytes(randomBytes);
+    String clientWebSocketKey = Base64.getEncoder().encodeToString(randomBytes);
+    writer.write("Sec-WebSocket-Key: " + clientWebSocketKey + "\n");
+
+    writer.write("Sec-WebSocket-Version: 13\n\n");
+    writer.flush();
+
+    Map<String, String> responseHeaders = readHandshake();
+    String serverAcceptKey = responseHeaders.getOrDefault("Sec-WebSocket-Accept", "");
+
+    if (responseHeaders.containsKey("HTTP/1.1 101 Switching Protocols") &&
+      "websocket".equals(responseHeaders.getOrDefault("Upgrade", "")) &&
+      "Upgrade".equals(responseHeaders.getOrDefault("Connection", ""))) {
+
+      String concatenatedKey = clientWebSocketKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+      byte[] sha1HashedKey = MessageDigest.getInstance("SHA1").digest(concatenatedKey.getBytes());
+      String computedAcceptKey = Base64.getEncoder().encodeToString(sha1HashedKey);
+
+      if (computedAcceptKey.equals(serverAcceptKey)) {
+        logger.info("[-] Client successfully connected to the server");
+        return true;
+      }
     }
 
-    public void run() {
-        new Thread(this::sendMessages, "Client Sender").start();
-        new Thread(this::reciveMessages, "Client Receiver").start();
+    logger.warn("[!] Error in incoming handshake: {}", responseHeaders);
+    return false;
+  }
+
+  @SneakyThrows
+  private Map<String, String> readHandshake() {
+    Map<String, String> requestHeaders = new HashMap<>();
+    String line;
+    while (!(line = reader.readLine()).isEmpty()) {
+      if (line.indexOf(": ") > 0) {
+        String[] headerParts = line.split(": ");
+        requestHeaders.put(headerParts[0], headerParts[1]);
+      } else {
+        requestHeaders.put(line, "");
+      }
     }
+    return requestHeaders;
+  }
 
-    @SneakyThrows
-    public void setupBuffered() {
-        consoleReader = new BufferedReader(new InputStreamReader(System.in));
-        writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+  @lombok.SneakyThrows
+  private void closeClient() {
+    this.shouldContinue = false;
+    consoleReader.close();
+
+    writer.write("\n");
+    writer.write((char) 0x88);
+    writer.flush();
+
+    reader.close();
+    writer.close();
+    socket.close();
+
+    logger.info("[-] Unsubscribed from server");
+  }
+
+  @lombok.SneakyThrows
+  private void sendMessages() {
+    String message = "";
+    while(shouldContinue) {
+      message = consoleReader.readLine();
+      if(message.equalsIgnoreCase("exit")) {
+        closeClient();
+      } else {
+        writer.write(message + '\n');
+        writer.flush();
+      }
     }
+  }
 
-    @SneakyThrows
-    public void performHandshake() {
-        setupBuffered();
-
-        writer.write("GET / HTTP/1.1");
-        writer.write("Upgrade:  websocket");
-        writer.write("http://localhost:" + port);
-
-        byte[] array = new byte[16];
-        new Random().nextBytes(array);
-        String encoded = new String(Base64.getEncoder().encode(array));
-        writer.write("Sec-WebSocket-Key: " + encoded);
-
-        writer.write("Sec-WebSocket-Version: 13");
-        writer.write("Connection: Upgrade\n\n");
+  @lombok.SneakyThrows
+  private void reciveMessages() {
+    String message = "";
+    while(shouldContinue && (message = reader.readLine()) != null) {
+      logger.info("[+] Message: " + message);
     }
-
-    @lombok.SneakyThrows
-    private void sendMessages() {
-        String message = "";
-        while(shouldContinue) {
-            message = consoleReader.readLine();
-            writer.write(message + '\n');
-            writer.flush();
-        }
-    }
-
-    @lombok.SneakyThrows
-    private void reciveMessages() {
-        String message = "";
-        while(shouldContinue) {
-            message = reader.readLine();
-            logger.info("[+] Message: " + message);
-        }
-    }
+  }
 }
