cddf7d2de7a7c550525b52c55adae3e02f8327ba
Kfiatek430 <110732078+Kfiatek430@users.noreply.github.com>
2025-03-25 22:49:39 +0100

    feat(client): add sendBytes and waitForConnection methods

--- Server/src/main/java/com/otsw/server/Main.java
+++ Server/src/main/java/com/otsw/server/Main.java
@@ -8,10 +8,6 @@ import java.io.InputStreamReader;
 public class Main {
   @SneakyThrows
   public static void main(String[] args) {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-    System.out.print("Enter port: ");
-    int port = Integer.parseInt(reader.readLine());
-    new Server(port);
+    new Server(8000);
   }
 }
--- TestingSoftware/src/main/java/com/otsw/testingsoftware/clients/TestingClient.java
+++ TestingSoftware/src/main/java/com/otsw/testingsoftware/clients/TestingClient.java
@@ -2,6 +2,7 @@ package com.otsw.testingsoftware.clients;
 
 import lombok.SneakyThrows;
 
+import java.util.Base64;
 import java.util.Random;
 
 public class TestingClient extends Client {
@@ -11,11 +12,18 @@ public class TestingClient extends Client {
 
   @SneakyThrows
   public void sendBytes(int count) {
-    byte[] randomBytes = new byte[16];
+    if(isConnected) {
+      logger.warn("[!] Client is not connected yet!");
+      return;
+    }
+
+    byte[] randomBytes = new byte[count];
     new Random().nextBytes(randomBytes);
 
-    for(byte b : randomBytes) {
-      sendByte(b);
-    }
+    String encoded = Base64.getEncoder().encodeToString(randomBytes);
+    writer.write(encoded + "\n");
+    writer.flush();
+
+    logger.info("[+] Sent {} bytes as Base64: {}", count, encoded);
   }
 }
