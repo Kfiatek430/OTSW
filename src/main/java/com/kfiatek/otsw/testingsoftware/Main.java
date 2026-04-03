56c73b9b80da0ef8f739e42344056926e6ebe63e
Kfiatek430 <michal.kwiatkowski.430@gmail.com>
2026-04-03 10:21:37 +0200

    feat(testing): load config from command line argument

--- src/main/java/com/kfiatek/otsw/testingsoftware/Main.java
+++ src/main/java/com/kfiatek/otsw/testingsoftware/Main.java
@@ -7,9 +7,14 @@ import lombok.SneakyThrows;
 public class Main {
   @SneakyThrows
   public static void main(String[] args) {
-    TestConfig config = ConfigLoader.load("testcases/multi_client_char_mix_test.yaml");
-//    TestConfig config = ConfigLoader.load("testcases/high_frequency_test.yaml");
-//    TestConfig config = ConfigLoader.load("testcases/stress_test_no_delay.yaml");
+    if (args.length == 0) {
+      System.err.println("Usage: java Main <testcase-url-or-path>");
+      System.err.println("Example: java Main testcases/multi_client_char_mix_test.yaml");
+      System.exit(1);
+    }
+    
+    String configPath = args[0];
+    TestConfig config = ConfigLoader.load(configPath);
     TestingSoftware testingSoftware = new TestingSoftware(config);
     testingSoftware.start();
   }
